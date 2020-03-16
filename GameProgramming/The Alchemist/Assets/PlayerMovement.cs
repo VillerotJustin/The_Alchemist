2eccd01ebe8c725beb1df236aa5f17b9296d15e0
maelhos <52194580+maelhos@users.noreply.github.com>
2020-03-16 08:56:40 +0100

    n

---
+++ GameProgramming/The Alchemist/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movment;
+    // Update is called once per frame
+    void Update()
+    {
+        // input
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movment.x);
+        animator.SetFloat("Vertical", movment.y);
+        animator.SetFloat("speed", movment.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movment * moveSpeed * Time.fixedDeltaTime);
+    }
+}
