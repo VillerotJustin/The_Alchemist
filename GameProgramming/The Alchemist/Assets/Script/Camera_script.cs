37e3d8bd1acad5a449099e63f98614d81878be5b
maelhos <52194580+maelhos@users.noreply.github.com>
2020-06-27 15:08:15 +0200

    new version

---
+++ GameProgramming/The Alchemist/Assets/Script/Camera_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform cam;
+    public Transform Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        cam.position = new Vector3(Player.position.x, Player.position.y, cam.position.z);
+    }
+}
---
+++ GameProgramming/The Alchemist/Assets/Script/Player_Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movspeed = 5f;
+    public Rigidbody2D rb;
+    public Animator anim;
+    Vector2 movement;
+    void Start()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x != 0)
+        {
+            anim.SetFloat("Horizontal_pre", movement.x);
+        }
+        if (movement.y != 0)
+        {
+            anim.SetFloat("Horizontal_pre", movement.y);
+        }
+
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movspeed * Time.fixedDeltaTime);
+    }
+}
