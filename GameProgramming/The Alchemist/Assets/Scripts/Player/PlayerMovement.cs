6315d5e0225e8777b85f95745f87abb951cb8640
helisoya <oudot.julien@laposte.net>
2022-06-22 15:51:50 +0200

    basic player movement

---
+++ GameProgramming/The Alchemist/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] Animator animator;
+    
+    Vector2 movement;
+
+    void Update()
+    {
+        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        if(! (movement.x == 0) || !(movement.y == 0)){
+            animator.SetFloat("Horizontal",movement.x);
+            animator.SetFloat("Vertical",movement.y);
+        }
+
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position+movement*speed*Time.fixedDeltaTime);
+    }
+}
