5252055e792454b5162a9fdd1e7ec739d81efb7d
helisoya <oudot.julien@laposte.net>
2022-07-21 15:17:10 +0200

    Time Compass
    Now including a shitty time compass :)

--- GameProgramming/The Alchemist/Assets/Scripts/World/Timehandler.cs
+++ GameProgramming/The Alchemist/Assets/Scripts/World/Timehandler.cs
@@ -6,9 +6,12 @@ using TMPro;
 public class Timehandler : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private RectTransform compass;
+
     void Update()
     {
         GameManager.instance.UpdateTime();
         text.text = GameManager.instance.inGameHour.ToString()+"H";
+        compass.eulerAngles = new Vector3(0,0,(360f/24f) * GameManager.instance.inGameHour);
     }
 }
