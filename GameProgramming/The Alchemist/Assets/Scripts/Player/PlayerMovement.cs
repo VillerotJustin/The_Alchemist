a294f4f6ac53ce39b77ce5900293ffe02e2292e1
helisoya <oudot.julien@laposte.net>
2022-08-08 16:27:23 +0200

    Updated Seasonal Tilesets + Camera
    New tiles for the tileset
    The camera now stays inbound the map
    Reworked how seasons works
    Started working on the first level

--- GameProgramming/The Alchemist/Assets/Scripts/Player/PlayerMovement.cs
+++ GameProgramming/The Alchemist/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 movement;
 
+    private float halfWidth;
+    private float halfHeight;
+
+    private Vector3Int min;
+    private Vector3Int max;
+    [SerializeField] private Tilemap bounds;
+
     void Awake(){
         Player.body = gameObject;
     }
@@ -20,6 +27,15 @@ public class PlayerMovement : MonoBehaviour
     public void ForceMove(float x,float y){
         movement.x = x;
         movement.y = y;
+    }
+
+
+    void Start(){
+        min = bounds.cellBounds.min;
+        max = bounds.cellBounds.max;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     void Update()
@@ -48,5 +64,9 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position+movement*GameManager.player.speed*Time.fixedDeltaTime);
 
         Camera.main.transform.position = new Vector3(transform.position.x,transform.position.y,-10);
+
+        float camX =  Mathf.Clamp(transform.position.x,min.x + halfWidth,max.x-halfWidth);;
+        float camY = Mathf.Clamp(transform.position.y,min.y + halfHeight,max.y-halfHeight);
+        Camera.main.transform.position = new Vector3(camX,camY,Camera.main.transform.position.z);
     }
 }
--- GameProgramming/The Alchemist/Assets/Scripts/World/TileMapSeasonModifier.cs
+++ GameProgramming/The Alchemist/Assets/Scripts/World/TileMapSeasonModifier.cs
@@ -6,10 +6,27 @@ using UnityEngine.Tilemaps;
 
 public class TileMapSeasonModifier : MonoBehaviour
 {
+    private static string[] seasons = {"spring","summer","autumn","winter"};
+    [SerializeField] private Tilemap[] maps;
+
     void Start()
     {
-        for(int i = 0; i < transform.childCount;i++){
-            transform.GetChild(i).gameObject.SetActive(i == GameManager.currentMonth);
+        string season = seasons[GameManager.currentMonth];
+        BoundsInt bounds;
+        TileBase tile;
+
+
+        foreach(Tilemap map in maps){
+            bounds = map.cellBounds;
+
+            for (int x = bounds.xMin; x < bounds.xMax; x++) {
+                for (int y = bounds.yMin; y < bounds.yMax; y++) {
+                    tile = map.GetTile(new Vector3Int(x,y,0));
+                    if (tile != null) {
+                        map.SetTile(new Vector3Int(x,y,0),Resources.Load<TileBase>("Tilesets/"+season+"/"+tile.name.Replace("spring",season)));
+                    }
+                }
+            }
         }
     }
 }
